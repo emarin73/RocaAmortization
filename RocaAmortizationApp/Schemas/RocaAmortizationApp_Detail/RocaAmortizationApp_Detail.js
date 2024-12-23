a7bb5793a38672811ed075f43e228c8d95b4554c
emarin73 <euclides.marin@rocanegras.com>
2024-12-22 19:06:08 -0500

    Amortization App Fresh

---
+++ RocaAmortizationApp/Schemas/RocaAmortizationApp_Detail/RocaAmortizationApp_Detail.js
@@ -0,0 +1,8 @@
+define('RocaAmortizationApp_Detail', [], function () {
+	return {
+		entitySchemaName: 'RocaLoanAmortizationTable',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
